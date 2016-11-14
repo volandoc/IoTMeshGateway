96b3a0e6572f15448b121d02e20c7a2bfcb8cefe
Yuriy Artym <yuriy.artym@globallogic.com>
2016-11-14 18:36:10 +0200

    Added Nest Thermostat devices type

--- plugins/NestDevicesPlugin/src/NestTypeFactory.cpp
+++ plugins/NestDevicesPlugin/src/NestTypeFactory.cpp
@@ -3,10 +3,15 @@
 
 std::string nestTypeNames[] = {
     "cameras",
+    "thermostats",
 };
 
 
 void NestTypeCamera::init() {
     NestType::init(nestTypeNames[NEST_TYPE_CAMERA]);
+}
+
+void NestTypeThermostat::init() {
+    NestType::init(nestTypeNames[NEST_TYPE_THERMOSTAT]);
 }
 
