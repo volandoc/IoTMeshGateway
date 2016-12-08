96edf0cbc89ec8204cd76fbf572cb46cb57cb314
Andrii Maslianyi <ansdrii.maslianyi@globallogic.com>
2016-12-08 13:58:12 +0200

    Added first hardware plugin based on ESP8266 and FreeRTOS

---
+++ HWPlugins/embgw_hw_plugin/include/private_ssid_config.h
@@ -0,0 +1,7 @@
+#ifndef __PRIVATE_SSID_CONFIG_H__
+#define __PRIVATE_SSID_CONFIG_H__
+
+#define WIFI_SSID "RPIAP_00000000af5ba267"
+#define WIFI_PASS "verizon123"
+
+#endif // __PRIVATE_SSID_CONFIG_H__
