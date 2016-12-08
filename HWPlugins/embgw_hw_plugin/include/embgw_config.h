c7b0af360c84e6718647c3ea45921fe238e86eb9
Andrii Maslianyi <ansdrii.maslianyi@globallogic.com>
2016-12-08 13:58:12 +0200

    Added first hardware plugin based on ESP8266 and FreeRTOS

---
+++ HWPlugins/embgw_hw_plugin/include/embgw_config.h
@@ -0,0 +1,16 @@
+#ifndef __EMBGW_CONFIG_H__
+#define __EMBGW_CONFIG_H__
+
+#define MQTT_HOST ("192.168.1.1")
+#define MQTT_PORT 1883
+
+#define MQTT_PUB_TOPIC "gw/Esp8266Plugin/event"
+#define MQTT_SUB_TOPIC "gw/Esp8266Plugin/command"
+
+#define GPIO_LED 2
+
+#define MQTT_USER NULL
+#define MQTT_PASS NULL
+#define PUB_MSG_LEN 32
+
+#endif // __EMBGW_CONFIG_H__
