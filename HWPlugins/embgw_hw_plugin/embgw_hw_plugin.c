240268b98f8fd7e6cfc39f67a199e52033c63745
Andrii Maslianyi <ansdrii.maslianyi@globallogic.com>
2016-12-12 09:27:31 +0200

    MQTT LastWill and JSON response added

--- HWPlugins/embgw_hw_plugin/embgw_hw_plugin.c
+++ HWPlugins/embgw_hw_plugin/embgw_hw_plugin.c
@@ -25,11 +25,11 @@ SemaphoreHandle_t wifi_alive;
 QueueHandle_t publish_queue;
 
 static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
-	if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
-			strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
-		return 0;
-	}
-	return -1;
+    if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
+            strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
+        return 0;
+    }
+    return -1;
 }
 
 static void execute_command(char * cmd) {
@@ -44,57 +44,57 @@ static void execute_command(char * cmd) {
 
 static void parse_command(char *command, size_t cmdsize) {
     int i;
-	int res;
-	jsmn_parser p;
-	jsmntok_t tokens[128]; /* We expect no more than 128 tokens */
-	char cmd[4];
-	jsmn_init(&p);
-	res = jsmn_parse(&p, command, cmdsize, tokens, sizeof(tokens)/sizeof(tokens[0]));
+    int res;
+    jsmn_parser p;
+    jsmntok_t tokens[128]; /* We expect no more than 128 tokens */
+    char cmd[4];
+    jsmn_init(&p);
+    res = jsmn_parse(&p, command, cmdsize, tokens, sizeof(tokens)/sizeof(tokens[0]));
     if (res < 0) {
-		printf("Failed to parse JSON: %d\n", res);
+        printf("Failed to parse JSON: %d\n", res);
         return;
-	}
+    }
     /* Assume the top-level element is an object */
-	if(res < 1 || tokens[0].type != JSMN_OBJECT) {
-		printf("Object expected\n");
+    if(res < 1 || tokens[0].type != JSMN_OBJECT) {
+        printf("Object expected\n");
         return;
-	}
+    }
     
-	/* Loop over all keys of the root object */
-	for (i = 1; i < res; i++) {
-		if (jsoneq(command, &tokens[i], "id") == 0) {
-			/* We may use strndup() to fetch string value */
-			printf("- id: %.*s\n", tokens[i+1].end-tokens[i+1].start,
-					command + tokens[i+1].start);
-			i++;
-		} else if (jsoneq(command, &tokens[i], "payload") == 0) {
-			/* We may use strndup() to fetch string value */
-			printf("- Payload: %.*s\n", tokens[i+1].end-tokens[i+1].start,
-					command + tokens[i+1].start);
-			i++;
-		} else if ((jsoneq(command, &tokens[i], "value") == 0) && ((jsoneq(command, &tokens[i+1], "on") == 0) || (jsoneq(command, &tokens[i+1], "off") == 0))) {
-			/* We may use strndup() to fetch string value */
-			printf("- value: %.*s\n", tokens[i+1].end-tokens[i+1].start,
-					command + tokens[i+1].start);
-			 snprintf(cmd, 4, "%.*s", tokens[i+1].end-tokens[i+1].start,
-					command + tokens[i+1].start);
-			execute_command(cmd);
-			i++;
-		} else if (jsoneq(command, &tokens[i], "reference") == 0) {
-			/* We may want to do strtol() here to get numeric value */
-			printf("- Reference: %.*s\n", tokens[i+1].end-tokens[i+1].start,
-					command + tokens[i+1].start);
-			i++;
-		} else if (jsoneq(command, &tokens[i], "timestamp") == 0) {
-			/* We may want to do strtol() here to get numeric value */
-			printf("- Timestamp: %.*s\n", tokens[i+1].end-tokens[i+1].start,
-					command + tokens[i+1].start);
-			i++;
-		} else {
-			printf("Unexpected key: %.*s\n", tokens[i].end-tokens[i].start,
-					command + tokens[i].start);
-		}
-	}
+    /* Loop over all keys of the root object */
+    for (i = 1; i < res; i++) {
+        if (jsoneq(command, &tokens[i], "id") == 0) {
+            /* We may use strndup() to fetch string value */
+            printf("- id: %.*s\n", tokens[i+1].end-tokens[i+1].start,
+                    command + tokens[i+1].start);
+            i++;
+        } else if (jsoneq(command, &tokens[i], "payload") == 0) {
+            /* We may use strndup() to fetch string value */
+            printf("- Payload: %.*s\n", tokens[i+1].end-tokens[i+1].start,
+                    command + tokens[i+1].start);
+            i++;
+        } else if ((jsoneq(command, &tokens[i], "value") == 0) && ((jsoneq(command, &tokens[i+1], "on") == 0) || (jsoneq(command, &tokens[i+1], "off") == 0))) {
+            /* We may use strndup() to fetch string value */
+            printf("- value: %.*s\n", tokens[i+1].end-tokens[i+1].start,
+                    command + tokens[i+1].start);
+             snprintf(cmd, 4, "%.*s", tokens[i+1].end-tokens[i+1].start,
+                    command + tokens[i+1].start);
+            execute_command(cmd);
+            i++;
+        } else if (jsoneq(command, &tokens[i], "reference") == 0) {
+            /* We may want to do strtol() here to get numeric value */
+            printf("- Reference: %.*s\n", tokens[i+1].end-tokens[i+1].start,
+                    command + tokens[i+1].start);
+            i++;
+        } else if (jsoneq(command, &tokens[i], "timestamp") == 0) {
+            /* We may want to do strtol() here to get numeric value */
+            printf("- Timestamp: %.*s\n", tokens[i+1].end-tokens[i+1].start,
+                    command + tokens[i+1].start);
+            i++;
+        } else {
+            printf("Unexpected key: %.*s\n", tokens[i].end-tokens[i].start,
+                    command + tokens[i].start);
+        }
+    }
 }
 
 static void beat_task(void *pvParameters) {
@@ -155,16 +155,32 @@ static void  mqtt_task(void *pvParameters) {
     struct mqtt_network network;
     mqtt_client_t client = mqtt_client_default;
     char mqtt_client_id[20];
-    uint8_t mqtt_buf[256];
-    uint8_t mqtt_readbuf[256];
+    uint8_t mqtt_buf[255];
+    uint8_t mqtt_readbuf[255];
+    mqtt_packet_will_options_t will = mqtt_packet_will_options_initializer;
     mqtt_packet_connect_data_t data = mqtt_packet_connect_data_initializer;
+
+	will.topicName.cstring = MQTT_WILL_TOPIC;
+	will.message.cstring = MQTT_WILL_MSG;
+	will.retained = 1;
+	will.qos = MQTT_QOS1;
 
     mqtt_network_new( &network );
     memset(mqtt_client_id, 0, sizeof(mqtt_client_id));
     strcpy(mqtt_client_id, "ESP-");
     strcat(mqtt_client_id, get_my_id());
 
+	data.willFlag = 1;
+	data.will = will;
+    data.MQTTVersion = 3;
+    data.clientID.cstring = mqtt_client_id;
+    data.username.cstring = MQTT_USER;
+    data.password.cstring = MQTT_PASS;
+    data.keepAliveInterval = 10;
+    data.cleansession = 0;
+
     while(1) {
+		vTaskDelay( 1000 / portTICK_PERIOD_MS );
         xSemaphoreTake(wifi_alive, portMAX_DELAY);
         printf("%s: started\n\r", __func__);
         printf("%s: (Re)connecting to MQTT server %s ... ",__func__,
@@ -176,15 +192,8 @@ static void  mqtt_task(void *pvParameters) {
             continue;
         }
         printf("done\n\r");
-        mqtt_client_new(&client, &network, 5000, mqtt_buf, 256,
-                      mqtt_readbuf, 256);
-        data.willFlag = 0;
-        data.MQTTVersion = 3;
-        data.clientID.cstring = mqtt_client_id;
-        data.username.cstring = MQTT_USER;
-        data.password.cstring = MQTT_PASS;
-        data.keepAliveInterval = 10;
-        data.cleansession = 0;
+        mqtt_client_new(&client, &network, 5000, mqtt_buf, 255,
+                      mqtt_readbuf, 255);
         printf("Send MQTT connect ... ");
         ret = mqtt_connect(&client, &data);
         if(ret){
@@ -193,6 +202,18 @@ static void  mqtt_task(void *pvParameters) {
             taskYIELD();
             continue;
         }
+
+        mqtt_message_t init_message;
+        init_message.payload = "conected";
+        init_message.payloadlen = 8;
+        init_message.dup = 0;
+        init_message.qos = MQTT_QOS1;
+        init_message.retained = 1;
+        ret = mqtt_publish(&client, MQTT_WILL_TOPIC, &init_message);
+        if (ret != MQTT_SUCCESS ){
+            printf("error while send status message: %d\n", ret );
+        }
+
         printf("done\r\n");
         mqtt_subscribe(&client, MQTT_SUB_TOPIC, MQTT_QOS1, topic_received);
         xQueueReset(publish_queue);
@@ -243,7 +264,7 @@ static void  wifi_task(void *pvParameters)
             printf("%s: status = %d\n\r", __func__, status );
             if( status == STATION_WRONG_PASSWORD ) {
                 printf("WiFi: wrong password\n\r");
-                break;
+                break;b
             } else if( status == STATION_NO_AP_FOUND ) {
                 printf("WiFi: AP not found\n\r");
                 break;
