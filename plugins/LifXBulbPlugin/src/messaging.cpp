3f63ad19b53e6de3b94aa96dcd8e82f083808ed8
Andrii Maslianyi <ansdrii.maslianyi@globallogic.com>
2016-12-26 10:39:50 +0200

    Fixes in sockets

--- plugins/LifXBulbPlugin/src/messaging.cpp
+++ plugins/LifXBulbPlugin/src/messaging.cpp
@@ -37,7 +37,7 @@ void LifxMessage::setPacketType(Poco::UInt16 type){
 }
 
 void LifxMessage::sendMessage(){
-    Poco::Net::NetworkInterface ni = Poco::Net::NetworkInterface::forName("ap0");
+    Poco::Net::NetworkInterface ni = Poco::Net::NetworkInterface::forName(ifname);
     sendMessage(ni.broadcastAddress().toString());
 }
 
@@ -71,8 +71,8 @@ void LifxMessage::sendMessage(std::string address){
     size_t size = messageBuffer.sizeBytes();
 
     try {
-        Poco::Net::SocketAddress sa(address, LifxPort);
-        socketUDP->sendTo(messageBuffer.begin(), size, sa);
+        //Poco::Net::SocketAddress sa(address, LifxPort);
+        socketUDP->sendBytes(messageBuffer.begin(), size);
     } catch(Poco::Exception excp) {
         logger.log(excp, __FILE__, 77);
     }
