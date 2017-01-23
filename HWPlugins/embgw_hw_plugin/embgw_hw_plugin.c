90726ae67b46a6f509afef68e03960a6ca9cb9a3
Andrii Maslianyi <ansdrii.maslianyi@globallogic.com>
2017-01-23 15:45:07 +0200

    Refactored all communication to new standard as single device

--- HWPlugins/embgw_hw_plugin/embgw_hw_plugin.c
+++ HWPlugins/embgw_hw_plugin/embgw_hw_plugin.c
@@ -28,6 +28,11 @@ QueueHandle_t gpio_queue;
 
 char sub_topic[64];
 
+const property_t led_property = {.name="LED", .type="string", .descr="Led indicator", .lim_type="enum", .lim_json="[\"on\",\"off\"]", .deflt="\"off\"", .rw="rw"};
+const property_t pir_property = {.name="PIR", .type="int", .descr="Motion sensor", .lim_type="", .lim_json="null", .deflt="0", .rw="r"};
+const property_t mic_property = {.name="MIC", .type="int", .descr="Noise detector", .lim_type="", .lim_json="null", .deflt="0", .rw="r"};
+
+
 irq_pin_t     PIR = {.last = 0, .prev = 0, .gpio = GPIO_PIR};
 irq_pin_t     MIC = {.last = 0, .prev = 0, .gpio = GPIO_MIC};
 control_pin_t LED = {.time_on = 0, .gpio = GPIO_LED};
@@ -37,53 +42,57 @@ control_pin_t LED = {.time_on = 0, .gpio = GPIO_LED};
 #define LED_ACTIVE (0)
 #define LED_TIMEOUT (15000)
 
-const char *json_template = "{\"id\":\"%s\","\
-"\"payload\": {"\
-"\"type\":\"event\","\
-"\"value\":\"SUCCESS\","\
-"\"cvalue\":\"PROPERTIES\","\
-"\"сontent\":["\
-"{\"name\":\"%s\","\
-"\"value\":\"%d\"}"\
-"]"\
-"},"\
-"\"reference\":\"1\","\
-"\"timestamp\" : %ld"\
+const char *property_template = "{\"name\":\"%s\","\
+"\"type\":\"%s\","\
+"\"descr\":\"%s\","\
+"\"lim_type\":\"%s\","\
+"\"lim_json\":%s,"\
+"\"default\":%s,"\
+"\"rw\":\"%s\""\
 "}";
+
+const char *status_template = "{\"status\":\"%s\","\
+"\"data\":%s,"\
+"\"time\":%ld,"\
+"\"err\":\"%s\""\
+"}";
+
+const char *event_template = "%s:%ld";
+
 
 typedef struct
 {
-	char *msg;
-	char *topic;
+    char *msg;
+    char *topic;
 }queue_buf_t;
 
 queue_buf_t pub_msg;
 
-void PIR_HANDLER(void)
+void PIR_HANDLER(uint8_t gpio_num)
 {
 
-	printf("interrupt gpio05 occurred\n");
-	if(xQueueSendToBackFromISR(gpio_queue, &PIR, NULL)== pdFALSE)
-		printf("Publish queue overflow.\r\n");
+    printf("interrupt gpio05 occurred\n");
+    if(xQueueSendToBackFromISR(gpio_queue, &PIR, NULL)== pdFALSE)
+        printf("Publish queue overflow.\r\n");
 
 }
 
-void MIC_HANDLER(void)
+void MIC_HANDLER(uint8_t gpio_num)
 {
-	uint32_t delta;
-	MIC.last = xTaskGetTickCountFromISR();
-	delta = MIC.last - MIC.prev;
-	if(delta > 100)
-	{
-		printf("interrupt gpio04 occurred\n");
-		MIC.prev = MIC.last;
-	    if(xQueueSendToBackFromISR(gpio_queue, &MIC, NULL)== pdFALSE)
-			printf("Publish queue overflow.\r\n");
-	}
-	else
-	{
-		printf("Jitter gpio04 detect exit\n");
-	}
+    uint32_t delta;
+    MIC.last = xTaskGetTickCountFromISR();
+    delta = MIC.last - MIC.prev;
+    if(delta > 100)
+    {
+        printf("interrupt gpio04 occurred\n");
+        MIC.prev = MIC.last;
+        if(xQueueSendToBackFromISR(gpio_queue, &MIC, NULL)== pdFALSE)
+            printf("Publish queue overflow.\r\n");
+    }
+    else
+    {
+        printf("Jitter gpio04 detect exit\n");
+    }
 
 }
 
@@ -127,32 +136,42 @@ static void parse_command(char *command, size_t cmdsize) {
     
     /* Loop over all keys of the root object */
     for (i = 1; i < res; i++) {
-        if (jsoneq(command, &tokens[i], "id") == 0) {
+        if (jsoneq(command, &tokens[i], "data") == 0) {
             /* We may use strndup() to fetch string value */
-            printf("- id: %.*s\n", tokens[i+1].end-tokens[i+1].start,
+            printf("- data: %.*s\n", tokens[i+1].end-tokens[i+1].start,
                     command + tokens[i+1].start);
             i++;
-        } else if (jsoneq(command, &tokens[i], "payload") == 0) {
+        } else if (jsoneq(command, &tokens[i], "PIR") == 0) {
             /* We may use strndup() to fetch string value */
-            printf("- Payload: %.*s\n", tokens[i+1].end-tokens[i+1].start,
+            printf("- PIR: %.*s\n", tokens[i+1].end-tokens[i+1].start,
                     command + tokens[i+1].start);
             i++;
-        } else if ((jsoneq(command, &tokens[i], "value") == 0) && ((jsoneq(command, &tokens[i+1], "on") == 0) || (jsoneq(command, &tokens[i+1], "off") == 0))) {
+        } else if (jsoneq(command, &tokens[i], "MIC") == 0) {
+            /* We may use strndup() to fetch string value */
+            printf("- PIR: %.*s\n", tokens[i+1].end-tokens[i+1].start,
+                    command + tokens[i+1].start);
+            i++;
+        } else if ((jsoneq(command, &tokens[i], "LED") == 0) && ((jsoneq(command, &tokens[i+1], "on") == 0) || (jsoneq(command, &tokens[i+1], "off") == 0))) {
             /* We may use strndup() to fetch string value */
             printf("- value: %.*s\n", tokens[i+1].end-tokens[i+1].start,
                     command + tokens[i+1].start);
-             snprintf(cmd, 4, "%.*s", tokens[i+1].end-tokens[i+1].start,
+            snprintf(cmd, 4, "%.*s", tokens[i+1].end-tokens[i+1].start,
                     command + tokens[i+1].start);
             execute_command(cmd);
             i++;
-        } else if (jsoneq(command, &tokens[i], "reference") == 0) {
+        } else if (jsoneq(command, &tokens[i], "time") == 0) {
             /* We may want to do strtol() here to get numeric value */
-            printf("- Reference: %.*s\n", tokens[i+1].end-tokens[i+1].start,
+            printf("- time: %.*s\n", tokens[i+1].end-tokens[i+1].start,
                     command + tokens[i+1].start);
             i++;
-        } else if (jsoneq(command, &tokens[i], "timestamp") == 0) {
+        } else if (jsoneq(command, &tokens[i], "id") == 0) {
             /* We may want to do strtol() here to get numeric value */
-            printf("- Timestamp: %.*s\n", tokens[i+1].end-tokens[i+1].start,
+            printf("- id: %.*s\n", tokens[i+1].end-tokens[i+1].start,
+                    command + tokens[i+1].start);
+            i++;
+        } else if (jsoneq(command, &tokens[i], "ref") == 0) {
+            /* We may want to do strtol() here to get numeric value */
+            printf("- ref: %.*s\n", tokens[i+1].end-tokens[i+1].start,
                     command + tokens[i+1].start);
             i++;
         } else {
@@ -206,24 +225,28 @@ static void  mqtt_task(void *pvParameters) {
     struct mqtt_network network;
     mqtt_client_t client = mqtt_client_default;
     char mqtt_client_id[20];
-    uint8_t mqtt_buf[255];
-    uint8_t mqtt_readbuf[255];
+    uint8_t mqtt_buf[511];
+    uint8_t mqtt_readbuf[511];
     mqtt_packet_will_options_t will = mqtt_packet_will_options_initializer;
     mqtt_packet_connect_data_t data = mqtt_packet_connect_data_initializer;
     queue_buf_t pub_msg_loc;
+    TickType_t xLastWakeTime;
 
-	will.topicName.cstring = MQTT_WILL_TOPIC;
-	will.message.cstring = MQTT_WILL_MSG;
-	will.retained = 1;
-	will.qos = MQTT_QOS1;
+    char will_topic[64];
+    sprintf(will_topic, MQTT_WILL_TOPIC, get_my_id());
+    
+    will.topicName.cstring = will_topic;
+    will.message.cstring = MQTT_WILL_MSG;
+    will.retained = 1;
+    will.qos = MQTT_QOS1;
 
     mqtt_network_new( &network );
     memset(mqtt_client_id, 0, sizeof(mqtt_client_id));
     strcpy(mqtt_client_id, "ESP-");
     strcat(mqtt_client_id, get_my_id());
 
-	data.willFlag = 1;
-	data.will = will;
+    data.willFlag = 1;
+    data.will = will;
     data.MQTTVersion = 3;
     data.clientID.cstring = mqtt_client_id;
     data.username.cstring = MQTT_USER;
@@ -232,7 +255,7 @@ static void  mqtt_task(void *pvParameters) {
     data.cleansession = 0;
 
     while(1) {
-		vTaskDelay( 1000 / portTICK_PERIOD_MS );
+        vTaskDelay( 1000 / portTICK_PERIOD_MS );
         xSemaphoreTake(wifi_alive, portMAX_DELAY);
         printf("%s: started\n\r", __func__);
         printf("%s: (Re)connecting to MQTT server %s ... ",__func__,
@@ -244,8 +267,8 @@ static void  mqtt_task(void *pvParameters) {
             continue;
         }
         printf("done\n\r");
-        mqtt_client_new(&client, &network, 5000, mqtt_buf, 255,
-                      mqtt_readbuf, 255);
+        mqtt_client_new(&client, &network, 5000, mqtt_buf, 511,
+                      mqtt_readbuf, 511);
         printf("Send MQTT connect ... ");
         ret = mqtt_connect(&client, &data);
         if(ret){
@@ -255,24 +278,42 @@ static void  mqtt_task(void *pvParameters) {
             continue;
         }
 
+        char tmp_property[PUB_MSG_LEN - 1] = "\0";
+        char properties[PUB_MSG_LEN - 1] = "\0";
+        strcat(properties,"[");
+        sprintf(tmp_property, property_template, led_property.name, led_property.type, led_property.descr, led_property.lim_type, led_property.lim_json, led_property.deflt, led_property.rw);
+        strcat(properties,tmp_property);
+        strcat(properties,",");
+        sprintf(tmp_property, property_template, pir_property.name, pir_property.type, pir_property.descr, pir_property.lim_type, pir_property.lim_json, pir_property.deflt, pir_property.rw);
+        strcat(properties,tmp_property);
+        strcat(properties,",");
+        sprintf(tmp_property, property_template, mic_property.name, mic_property.type, mic_property.descr, mic_property.lim_type, mic_property.lim_json, mic_property.deflt, mic_property.rw);
+        strcat(properties,tmp_property);
+        strcat(properties,"]");
+
+        char msg[PUB_MSG_LEN - 1] = "\0";
+        xLastWakeTime = xTaskGetTickCount();
+        sprintf(msg, status_template, "available", properties, xLastWakeTime,"null");
+        printf("%s: connection message is %s \n with length %d ",__func__, msg, strlen(msg));
+        
         mqtt_message_t init_message;
-        init_message.payload = "conected";
-        init_message.payloadlen = strlen("conected");
+        init_message.payload = msg;
+        init_message.payloadlen = strlen(msg);
         init_message.dup = 0;
         init_message.qos = MQTT_QOS1;
         init_message.retained = 1;
-        ret = mqtt_publish(&client, MQTT_WILL_TOPIC, &init_message);
+        ret = mqtt_publish(&client, will_topic, &init_message);
         if (ret != MQTT_SUCCESS ){
             printf("error while send status message: %d\n", ret );
         }
 
         printf("done\r\n");
-        sprintf(sub_topic, MQTT_SUB_TOPIC, "LED");
+        sprintf(sub_topic, MQTT_SUB_TOPIC, get_my_id());
         mqtt_subscribe(&client, sub_topic, MQTT_QOS1, topic_received);
+
         xQueueReset(publish_queue);
 
         while(1){
-            char msg[PUB_MSG_LEN - 1] = "\0";
             while(xQueueReceive(publish_queue, &pub_msg_loc, 0) == pdTRUE){
                 printf("got message to publish\r\n");
                 mqtt_message_t message;
@@ -292,10 +333,10 @@ static void  mqtt_task(void *pvParameters) {
 
             if(gpio_read(LED.gpio) == LED_ACTIVE)
             {
-            	if((xTaskGetTickCount() - LED.time_on) > LED_TIMEOUT)
-            	{
-            		gpio_write(LED.gpio, 1);
-            	}
+                if((xTaskGetTickCount() - LED.time_on) > LED_TIMEOUT)
+                {
+                    gpio_write(LED.gpio, 1);
+                }
             }
 
 
@@ -357,53 +398,51 @@ static void  wifi_task(void *pvParameters)
 
 void gpio_task(void *pvParameters)
 {
-    int count = 0;
-	gpio_set_interrupt(GPIO_PIR, GPIO_INTTYPE_EDGE_POS);
-	gpio_set_interrupt(GPIO_MIC, GPIO_INTTYPE_EDGE_POS);
+    gpio_set_interrupt(GPIO_PIR, GPIO_INTTYPE_EDGE_POS, PIR_HANDLER);
+    gpio_set_interrupt(GPIO_MIC, GPIO_INTTYPE_EDGE_POS, MIC_HANDLER);
 
     TickType_t xLastWakeTime = xTaskGetTickCount();
 
-	for(;;)
-	{
-		irq_pin_t gpio_pin;
-		xQueueReceive(gpio_queue, &gpio_pin, portMAX_DELAY);
-		xLastWakeTime = xTaskGetTickCount();
-		char *msg = malloc(256);
-		char *topic = malloc(64);
+    for(;;)
+    {
+        irq_pin_t gpio_pin;
+        xQueueReceive(gpio_queue, &gpio_pin, portMAX_DELAY);
+        xLastWakeTime = xTaskGetTickCount();
+        char *msg = malloc(256);
+        char *topic = malloc(64);
 
-		if(msg == NULL)
-		{
-			printf("[ERROR allocate memory\n]");
-			return;
-		}
+        if(msg == NULL)
+        {
+            printf("[ERROR allocate memory\n]");
+            return;
+        }
 
-		if(gpio_pin.gpio == GPIO_PIR)
-		{
-			sprintf(msg, json_template, "PIR01", "PIR", 1, xLastWakeTime);
-			sprintf(topic, MQTT_PUB_TOPIC, "PIR_SENSOR");
+        if(gpio_pin.gpio == GPIO_PIR)
+        {
+            sprintf(msg, event_template, "1", xLastWakeTime);
+            sprintf(topic, MQTT_PUB_TOPIC, get_my_id(), "PIR");
 
-		}
-		else if (gpio_pin.gpio == GPIO_MIC)
-		{
-			sprintf(msg, json_template, "MIC01", "MIC", 1, xLastWakeTime);
-			sprintf(topic, MQTT_PUB_TOPIC, "MIC_SENSOR");
-		}
-		else
-		{
-			printf("unidentified sensor\n");
-			free(msg);
-			free(topic);
-		}
+        }
+        else if (gpio_pin.gpio == GPIO_MIC)
+        {
+            sprintf(msg, event_template, "1", xLastWakeTime);
+            sprintf(topic, MQTT_PUB_TOPIC, get_my_id(), "MIC");
+        }
+        else
+        {
+            printf("unidentified sensor\n");
+            free(msg);
+            free(topic);
+        }
 
-		pub_msg.msg = msg;
-		pub_msg.topic = topic;
+        pub_msg.msg = msg;
+        pub_msg.topic = topic;
 
-		if (xQueueSend(publish_queue, &pub_msg, 0) == pdFALSE)
-			printf("Publish queue overflow.\r\n");
+        if (xQueueSend(publish_queue, &pub_msg, 0) == pdFALSE)
+            printf("Publish queue overflow.\r\n");
 
-	}
+    }
 }
-
 
 
 void user_init(void)
--- HWPlugins/embgw_hw_plugin/include/embgw_config.h
+++ HWPlugins/embgw_hw_plugin/include/embgw_config.h
@@ -4,14 +4,13 @@
 #define MQTT_HOST ("172.24.254.20") //DragonBoard
 #define MQTT_PORT 1883
 
-#define MQTT_PUB_TOPIC "A000000000000777/Esp8266Plugin/%s/event"
+#define MQTT_PUB_TOPIC "A000000000000777/Esp8266Plugin/%s/%s/event"
 #define MQTT_SUB_TOPIC "A000000000000777/Esp8266Plugin/%s/command"
-#define MQTT_WILL_TOPIC "A000000000000777/Esp8266Plugin/status"
-#define MQTT_WILL_MSG "disconected"
-
+#define MQTT_WILL_TOPIC "A000000000000777/Esp8266Plugin/%s/status"
+#define MQTT_WILL_MSG "{\"status\":\"offline\", \"data\":null, \"time\":null, \"err\":\"Driver not running\"}"
 
 #define MQTT_USER NULL
 #define MQTT_PASS NULL
-#define PUB_MSG_LEN 256
+#define PUB_MSG_LEN 512
 
 #endif // __EMBGW_CONFIG_H__
